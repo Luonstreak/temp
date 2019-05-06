96ed5e45aa11acb11b993c69b99759aabe8fb7d5
Lucio Rodriguez <luonstreak@gmail.com>
2019-05-06 16:58:42 -0700

    adds skills info and work personal projects info

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
-<html>
-  <head>
-    <link rel="stylesheet" type="html/css" href="./index.css" />
-    <script src="./index.js" />
-    <title>LUCIO RODRIGUEZ</title>
-  </head>
-  <body>
-    <h1>HELLO</h1>
-  </body>
-</html>;
+var squares = document.querySelectorAll(".stk-square");
+var icons = document.querySelectorAll(".contact-icon");
+(function() {
+  for (var el of squares) {
+    el.addEventListener("mouseenter", function() {
+      var color = this.getAttribute("color");
+      if (color === "white" || color === "yellow") {
+        this.style.color = "darkslategrey";
+      } else {
+        this.style.color = "white";
+      }
+      this.style.border = "4px solid transparent";
+      this.style.backgroundColor = color;
+    });
+  }
+
+  for (var el of icons) {
+    el.addEventListener("mouseenter", function() {
+      var color = this.getAttribute("color");
+      this.style.color = color;
+    });
+    el.addEventListener("mouseleave", function() {
+      this.style.color = "#555152";
+    });
+  }
+})();
