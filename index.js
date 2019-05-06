c816c10cb16814a911171c4c89cf4be2e29f8678
Lucio Rodriguez <luonstreak@gmail.com>
2019-05-06 13:57:21 -0700

    inital commit

---
+++ index.js
@@ -0,0 +1,10 @@
+<html>
+  <head>
+    <link rel="stylesheet" type="html/css" href="./index.css" />
+    <script src="./index.js" />
+    <title>LUCIO RODRIGUEZ</title>
+  </head>
+  <body>
+    <h1>HELLO</h1>
+  </body>
+</html>;
